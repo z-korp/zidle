89316c0926113893e97b2101fbd766dbeae6faad
Cheelax <thomas.belloc@gmail.com>
2024-08-28 11:50:55 -0400

    fix locked sub ressource

--- client/src/utils/level.tsx
+++ client/src/utils/level.tsx
@@ -1,26 +1,30 @@
-// xpLevelUtils.ts
-
 // Constants
 const MAX_LEVEL = 99;
 const BASE_XP = 100;
 const CURVE_FACTOR = 10; // Adjusts the steepness of the curve
 
 // Function to calculate XP required for a given level
-function xpForLevel(level: number): number {
-    return Math.floor(BASE_XP + CURVE_FACTOR * (Math.pow(2, level) - 1));
+export function xpForLevel(level: number): number {
+    if (level === 0) {
+        return 0;
+    }
+    // Utilisation de BigInt pour éviter les problèmes de précision avec les grands nombres
+    return Number(BigInt(BASE_XP) + BigInt(CURVE_FACTOR) * BigInt(level) * BigInt(level));
 }
 
-// Function to get level from XP
+// Corrected function to get level from XP
 export function getLevelFromXp(xp: number): number {
     if (xp < BASE_XP + CURVE_FACTOR) {
         return 0;
     }
 
     let level = 1;
-    while (level < MAX_LEVEL && xp >= xpForLevel(level + 1)) {
+    while (true) {
+        if (level === MAX_LEVEL || xp < xpForLevel(level + 1)) {
+            break;
+        }
         level += 1;
     }
-
     return level;
 }
 
