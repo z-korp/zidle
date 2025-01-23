ea0d5de754a32bf8413f72a061d5928944c4b150
Cosmos 612 <cosmos.dev612@gmail.com>
2025-01-23 14:13:47 +0100

    Btn streaming

--- client/src/ui/screens/Home.tsx
+++ client/src/ui/screens/Home.tsx
@@ -5,6 +5,9 @@ import { useQuerySync } from "@dojoengine/react";
 import MainMenuCard from "../components/MainMenuCard";
 import { Card, CardHeader, CardContent } from "../elements/card";
 import CharacterList from "../components/CharacterList";
+import { Button } from "@/ui/elements/button";
+import { Store, Radio } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
   const {
@@ -14,15 +17,27 @@ export const Home = () => {
   useQuerySync(toriiClient, contractModels as any, []);
 
   const [selectedNft, setSelectedNft] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   return (
     <div className="relative flex flex-col h-screen">
       <Header />
-      <div className="relative flex flex-col gap-8 grow items-center justify-start ">
+      <div className="relative flex flex-col gap-8 grow items-center justify-start">
         <div className="absolute flex flex-col items-center gap-4 w-full p-2 max-w-4xl">
           {selectedNft == null ? (
             <Card className="w-[350px] bg-gray-800 text-white shadow-xl border border-gray-600">
-              <CardHeader className="p-3"></CardHeader>
+              <CardHeader className="p-3">
+                <div className="flex justify-end space-x-2">
+                  <Button
+                    variant="outline"
+                    onClick={() => navigate("/streaming")}
+                    className="text-white"
+                  >
+                    <Radio className="w-4 h-4 mr-2" />
+                    Streaming
+                  </Button>
+                </div>
+              </CardHeader>
               <CardContent>
                 <CharacterList onCharacterSelect={setSelectedNft} />
               </CardContent>
